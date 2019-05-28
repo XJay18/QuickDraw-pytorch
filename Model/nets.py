9f87385e8e8df8c283fc103db3a6e9feb2515fec
XJay18 <XJay18@users.noreply.github.com>
2019-05-28 22:12:10 +0800

    add resnet18 api

--- Model/nets.py
+++ Model/nets.py
@@ -3,6 +3,17 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+def resnet18(numclasses, pretrained=False):
+    model = models.resnet18(pretrained)
+    conv1_out_channels = model.conv1.out_channels
+    model.conv1 = nn.Conv2d(1, conv1_out_channels, kernel_size=3,
+                            stride=1, padding=1, bias=False)
+    model.maxpool = nn.MaxPool2d(kernel_size=2)
+    fc_features = model.fc.in_features
+    model.fc = nn.Linear(fc_features, numclasses)
+    return model
+
+
 def resnet34(numclasses, pretrained=False):
     model = models.resnet34(pretrained)
     conv1_out_channels = model.conv1.out_channels
@@ -47,6 +58,7 @@ class ConvNet(nn.Module):
         net = net.view(net.size(0), -1)
         net = self.fc(net)
         return net
+
 
 def convnet(numclasses):
     return ConvNet(numclasses)
